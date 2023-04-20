83af4999313963a99c21aaf1a0a8ad81b386412f
kitrath <crathmel@gmail.com>
2023-04-19 20:43:48 -0500

    add reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,7 +2,31 @@ const { Schema } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema(
-
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
 );
 
 module.exports = reactionSchema;
